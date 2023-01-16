0db63ea817940d105b51ca7c1e6ec3b43fbac1c9
turan <turan6677@gmail.com>
2023-01-16 21:34:54 +0300

    MainExtension.swift created.

--- AlbumsApp/Controller/MainViewController.swift
+++ AlbumsApp/Controller/MainViewController.swift
@@ -14,6 +14,9 @@ class MainViewController: UIViewController {
     @IBOutlet weak var mainTableView: UITableView!
     
     let viewModel: AlbumsMainViewModel
+    var menuView = UIView(frame: CGRect(x: 0, y: 100, width: 250, height: 718))
+    let aboutMeButton = UIButton(frame: CGRect(x: 50, y: 650, width: 150, height: 40))
+    
     
     init(viewModel: AlbumsMainViewModel) {
         self.viewModel = viewModel
@@ -30,49 +33,16 @@ class MainViewController: UIViewController {
         mainTableView.delegate = self
         mainTableView.dataSource = self
         fetchData()
-        
+        loadMenuView()
+        aboutMeButtonClicked()
+  
     }
-    func fetchData() {
-        viewModel.fetchData { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let items):
-                DispatchQueue.main.async {
-                    self.viewModel.uniqueElement = items
-                    self.mainTableView.reloadData()
-                }
-            case .failure(let error):
-                print(error)
-            }
+    @IBAction func menuButtonClicked(_ sender: Any) {
+        if menuView.isHidden {
+            menuView.isHidden = false
+        } else {
+            menuView.isHidden = true
         }
     }
     
-}
-extension MainViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return groupedUniqueElements.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "albumCellIdentifier", for: indexPath) as! MainTableViewCell
-        let elements = groupedUniqueElements[groupedUniqueElements.keys.sorted()[indexPath.row]]
-        let currentAlbum = (elements?.first)!
-        cell.configCell(model: currentAlbum)
-        return cell
-        
-    }
-}
-extension MainViewController {
-    var groupedUniqueElements: [Int: [UniqueElement]] {
-        var groupedElements: [Int: [UniqueElement]] = [:]
-        for element in viewModel.uniqueElement {
-            if groupedElements[element.album.id] == nil {
-                groupedElements[element.album.id] = [element]
-            } else {
-                groupedElements[element.album.id]?.append(element)
-            }
-        }
-        return groupedElements
-    }
 }
